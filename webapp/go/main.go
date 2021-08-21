5b87c274e734fefb6d99799086ce32afe4d30ae5
polamjag <s@polamjag.info>
2021-08-21 18:41:59 +0900

    db.SetMaxOpenConns(100)
    Co-Authored-By: pastak <pasta0915@gmail.com>

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -252,7 +252,7 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(100)
 	defer db.Close()
 
 	postIsuConditionTargetBaseURL = os.Getenv("POST_ISUCONDITION_TARGET_BASE_URL")
